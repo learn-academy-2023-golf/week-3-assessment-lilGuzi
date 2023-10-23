7921fd33613fdedb5a014b578b7079d73dcf54fb
jeffguzi <jeffreystephano@gmail.com>
2023-10-23 08:53:52 -0700

    adds all assessments

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,7 @@
 // ASSESSMENT 3: Coding Practical Questions with Jest
 
+const { describe } = require("yargs")
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -13,16 +15,32 @@
 // --------------------1) Prompt: Create a function that takes in a number (greater than 2) and returns an array containing the Fibonacci sequence. The length of the array is determined by the argument of the function.
 
 // a) Create a test with expect statements for each of the variables provided.
-
-const fibonacciLength1 = 6
-// Expected output: [1, 1, 2, 3, 5, 8]
-
-const fibonacciLength2 = 10
-// Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+describe("fiboSeq", () => {
+  it("takes in a number (greater than 2) and returns an array containing the Fibonacci sequence", () => {
+    const fibonacciLength1 = 6
+    // Expected output: [1, 1, 2, 3, 5, 8]
+    const fibonacciLength2 = 10
+    // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+    expect(fiboSeq(fibonacciLength1)).toEqual([1, 1, 2, 3, 5, 8]);
+    expect(fiboSeq(fibonacciLength2)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55]);
+  })
+})
 
 // b) Create the function that makes the test pass.
+// Pseudo code: 
+// Declare a function called fiboSeq
+// parameter will be the fibonacciLength
+// use 
 
-// Pseudo code:
+const fiboSeq = (number) => {
+  if(number > 2){
+    var fiboArr = (fiboSeq - 1)
+    fiboArr.push(fiboArr[fiboArr.length - 1] + fiboArr[fiboArr.length - 2])
+    return fiboArr
+    }
+  }
+
+
 
 // --------------------2) For the following prompt, use ChatGPT or an equivalent AI tool to help you solve the problem. The prompt will require the use of a JavaScript method called Object.values()
 // Prompt: Create a function that takes in an object and returns an array of the object's values sorted from least to greatest.
@@ -54,3 +72,9 @@ const studyMinutesWeek2 = {
 // b) Create the function that makes the test pass.
 
 // Pseudo code:
+
+
+
+
+
+// i Give up on this challenege, sunday oct 22 10:22pm  i keep gettin "your test suite must conatin at least one test" in terminal.
--- interview-questions.md
+++ interview-questions.md
@@ -8,21 +8,21 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is JSX? What is one syntax difference between HTML and JSX?
 
-Your answer:
+Your answer: JSX is JavaScript XML which allows you to use HTML-like code within JavaScript
 
-Researched answer:
+Researched answer:JSX stands for JavaScript XML which is an extention in JavaScript that allows you to use HTML-like code withing JavaScript 
 
 2. What is yarn? What file(s) are modified when you run the command yarn in your terminal?
 
-Your answer:
+Your answer: i honestly dont know 
 
-Researched answer:
+Researched answer: Yarn is a package manager for javascript. when you run the yarn command, package.json, and yarn.lock are files that are modified. 
 
 3. What is the difference between state values and props in React?
 
-Your answer:
+Your answer: id say state is data that is specific to a component and props is data that can be passed from one component to another.
 
-Researched answer:
+Researched answer: state is used to manage internal component data and can be changed during the lifecycle of the component. props are used to pass data from a parent component to a child component.
 
 4. STRETCH: Which is the difference between a div tag and a span tag?
 
